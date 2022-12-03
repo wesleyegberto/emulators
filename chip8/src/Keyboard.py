72b0dd5dd7f7ea5f50c842882a8c085258a43fa5
Wesley Egberto <wesleyegberto@gmail.com>
2022-12-03 15:11:32 -0300

    implemented Keyboard using Pygame keyboard events

--- chip8/src/Keyboard.py
+++ chip8/src/Keyboard.py
@@ -1,3 +1,11 @@
+import pygame
+
+PYGAME_EVENT_MAPPING = {
+    pygame.K_4: 0x1, pygame.K_5: 0x2, pygame.K_6: 0x3, pygame.K_7: 0xC,
+    pygame.K_r: 0x4, pygame.K_t: 0x5, pygame.K_y: 0x6, pygame.K_u: 0xD,
+    pygame.K_f: 0x7, pygame.K_g: 0x8, pygame.K_h: 0x9, pygame.K_j: 0xE,
+    pygame.K_v: 0xA, pygame.K_b: 0x0, pygame.K_n: 0xB, pygame.K_m: 0xF
+}
 
 class Keyboard:
     """ Class to handle keyboard events.
@@ -8,6 +16,13 @@ class Keyboard:
     4 5 6 D
     7 8 9 E
     A 0 B F
+
+    Keyboard controller layout:
+
+    4 5 6 7
+    R T Y U
+    F G H J
+    V B N M
     """
     def __init__(self):
         self.last_key_pressed = None
@@ -18,5 +33,14 @@ class Keyboard:
     def send_key(self, key_code):
         self.last_key_pressed = key_code & 0xF
 
-    def handle_pygame_event(self, event):
-        pass
+    def handle_pygame_event(self, event) -> bool:
+        if event.type != pygame.KEYDOWN:
+            return False
+
+        if event.key not in PYGAME_EVENT_MAPPING:
+            return False;
+
+        key_value = PYGAME_EVENT_MAPPING.get(event.key)
+        self.send_key(key_value)
+        return True
+
--- chip8/test/keyboard_test.py
+++ chip8/test/keyboard_test.py
@@ -1,10 +1,11 @@
-import unittest
 import sys
-from pathlib import Path
+import unittest
+import pygame
+from pygame.event import Event
 
 sys.path.append('src')
 
-from Keyboard import Keyboard
+from Keyboard import Keyboard, PYGAME_EVENT_MAPPING
 
 class KeyboardTest(unittest.TestCase):
     def setUp(self):
@@ -18,3 +19,19 @@ class KeyboardTest(unittest.TestCase):
 
         self.assertEqual(self.keyboard.read_key(), 0x9)
 
+    def test_should_ignore_unwanted_pygame_event(self):
+        event = Event(pygame.QUIT)
+        handled = self.keyboard.handle_pygame_event(event)
+        self.assertFalse(handled)
+
+    def test_should_ignore_unmapped_key(self):
+        event = Event(pygame.KEYDOWN, key=pygame.K_a)
+        handled = self.keyboard.handle_pygame_event(event)
+        self.assertFalse(handled)
+
+    def test_should_set_pressed_key(self):
+        for k,v in PYGAME_EVENT_MAPPING.items():
+            event = Event(pygame.KEYDOWN, key=k)
+            handled = self.keyboard.handle_pygame_event(event)
+            self.assertTrue(handled)
+            self.assertEqual(self.keyboard.read_key(), v)
