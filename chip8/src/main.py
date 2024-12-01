8c3c470b1c46f104c85218c394e8470d61226b8d
Wesley Egberto <wesleyegberto@gmail.com>
2024-11-30 21:59:17 -0300

    update example

--- chip8/src/main.py
+++ chip8/src/main.py
@@ -14,24 +14,26 @@ def main():
     cpu = Cpu(memory, display, keyboard)
 
     # test program
-    memory.write_16bit(0x200, 0xA21C)
+    memory.write_16bit(0x200, 0xA216)
 
-    memory.write_16bit(0x202, 0x613F)
-    memory.write_16bit(0x204, 0x621F)
-    memory.write_16bit(0x206, 0xD122)
-    memory.write_16bit(0x208, 0x1208)
-
-    # memory.write_16bit(0x202, 0x613C)
-    # memory.write_16bit(0x204, 0x621E)
-    # memory.write_16bit(0x206, 0xD125)
+    # dot warped in the 4 corners
+    # memory.write_16bit(0x200, 0xA21C)
+    # memory.write_16bit(0x202, 0x613F)
+    # memory.write_16bit(0x204, 0x621F)
+    # memory.write_16bit(0x206, 0xD122)
     # memory.write_16bit(0x208, 0x1208)
 
-    # memory.write_16bit(0x208, 0x613F)
-    # memory.write_16bit(0x20A, 0x6205)
-    # memory.write_16bit(0x20C, 0xD125)
-    # memory.write_16bit(0x20E, 0x6138)
-    # memory.write_16bit(0x210, 0x621D)
-    # memory.write_16bit(0x212, 0xD125)
+    memory.write_16bit(0x202, 0x6100)
+    memory.write_16bit(0x204, 0x6200)
+    memory.write_16bit(0x206, 0xD125)
+    memory.write_16bit(0x208, 0x1208)
+
+    memory.write_16bit(0x208, 0x613F)
+    memory.write_16bit(0x20A, 0x6205)
+    memory.write_16bit(0x20C, 0xD125)
+    memory.write_16bit(0x20E, 0x6138)
+    memory.write_16bit(0x210, 0x621D)
+    memory.write_16bit(0x212, 0xD125)
 
     memory.write_16bit(0x214, 0x1214)
     memory.write_16bit(0x216, 0xFF99)
