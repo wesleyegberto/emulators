52c2720a4a941362768c622d1d345f88d4e5e02c
Wesley Egberto <wesleyegberto@gmail.com>
2024-12-24 20:04:44 -0300

    update keyboard keys

--- chip8/src/Keyboard.py
+++ chip8/src/Keyboard.py
@@ -2,10 +2,10 @@ import pygame
 from utils import quit_emulator
 
 PYGAME_EVENT_MAPPING = {
-    pygame.K_4: 0x1, pygame.K_5: 0x2, pygame.K_6: 0x3, pygame.K_7: 0xC,
-    pygame.K_r: 0x4, pygame.K_t: 0x5, pygame.K_y: 0x6, pygame.K_u: 0xD,
-    pygame.K_f: 0x7, pygame.K_g: 0x8, pygame.K_h: 0x9, pygame.K_j: 0xE,
-    pygame.K_v: 0xA, pygame.K_b: 0x0, pygame.K_n: 0xB, pygame.K_m: 0xF
+    pygame.K_1: 0x1, pygame.K_2: 0x2, pygame.K_3: 0x3, pygame.K_4: 0xC,
+    pygame.K_q: 0x4, pygame.K_w: 0x5, pygame.K_e: 0x6, pygame.K_r: 0xD,
+    pygame.K_a: 0x7, pygame.K_s: 0x8, pygame.K_d: 0x9, pygame.K_f: 0xE,
+    pygame.K_z: 0xA, pygame.K_x: 0x0, pygame.K_c: 0xB, pygame.K_v: 0xF
 }
 
 class Keyboard:
@@ -20,10 +20,10 @@ class Keyboard:
 
     Keyboard controller layout:
 
-    4 5 6 7
-    R T Y U
-    F G H J
-    V B N M
+    1 2 3 4
+    Q W E R
+    A S D F
+    Z X C V
     """
 
     def __init__(self, mocked = False):
